70b76c23dfb9eb3460ebb744ad037df1fd085ada
Samuel Jansem <samuel.jansem@gmail.com>
2025-02-25 18:48:23 -0300

    enhance clipboard copy functionality

--- git-fzf-branch.sh
+++ git-fzf-branch.sh
@@ -11,7 +11,15 @@ selected_branch=$(
       --preview "git log --oneline --graph --color=always {} 2>/dev/null || git log --oneline --graph --color=always origin/{} 2>/dev/null" \
       --preview-window=right:60%:wrap \
       --bind "ctrl-r:reload(git fetch --all --prune && git for-each-ref --sort=-committerdate --format='%(refname:short)' refs/heads refs/remotes/origin | sed -E 's#^origin/##' | grep -v '^origin$' | awk '!seen[\$0]++')" \
-      --bind "ctrl-y:execute(echo -n {} | { pbcopy 2>/dev/null || xclip -selection clipboard 2>/dev/null || wl-copy 2>/dev/null; })+abort"
+      --bind "ctrl-y:execute(
+        echo -n {} | { 
+          command -v pbcopy &>/dev/null && pbcopy || \
+          command -v xclip &>/dev/null && xclip -selection clipboard || \
+          command -v wl-copy &>/dev/null && wl-copy || \
+          command -v clip.exe &>/dev/null && clip.exe || \
+          echo 'Clipboard copy not supported' >&2 
+        }
+      )+abort"
 )
 
 if [ -n "$selected_branch" ]; then
