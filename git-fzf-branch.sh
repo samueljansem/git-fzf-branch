046b86a089e992758d1ec16db020cb30771ab2ec
Samuel Jansem <samuel.jansem@gmail.com>
2025-12-09 14:11:53 -0300

    refactor branch selection to improve uniqueness and enhance preview functionality

--- git-fzf-branch.sh
+++ git-fzf-branch.sh
@@ -23,22 +23,24 @@ if [ $# -eq 1 ]; then
 fi
 
 selected_branch=$(
-  git for-each-ref --sort=-committerdate --format='%(refname:short)' refs/heads refs/remotes/origin | sed -E 's#^origin/##' | grep -v '^origin$' | awk '!seen[$0]++' |
-    fzf --height=50% --border --layout=reverse \
-      --preview "git log --oneline --graph --color=always {} 2>/dev/null || git log --oneline --graph --color=always origin/{} 2>/dev/null" \
+  git for-each-ref --sort=-committerdate --format='%(refname:short)|%(committerdate:relative)' refs/heads refs/remotes/origin |
+  sed -E 's#^origin/##' | awk -F'|' '$1 != "origin" && !seen[$1]++' | column -t -s '|' |
+    fzf --height=50% --border --layout=reverse --nth 1 \
+      --preview "git show --color=always --stat {1} 2>/dev/null || git show --color=always --stat origin/{1} 2>/dev/null" \
       --preview-window=right:60%:wrap \
-      --bind "ctrl-r:reload(git fetch --all --prune && git for-each-ref --sort=-committerdate --format='%(refname:short)' refs/heads refs/remotes/origin | sed -E 's#^origin/##' | grep -v '^origin$' | awk '!seen[\$0]++')" \
+      --bind "ctrl-r:reload(git fetch --all --prune && git for-each-ref --sort=-committerdate --format='%(refname:short)|%(committerdate:relative)' refs/heads refs/remotes/origin | sed -E 's#^origin/##' | awk -F'|' '\$1 != \"origin\" && !seen[\$1]++' | column -t -s '|')" \
       --bind "ctrl-y:execute(
-        echo -n {} | { 
+        echo -n {1} | { 
           command -v pbcopy &>/dev/null && pbcopy || \
           command -v xclip &>/dev/null && xclip -selection clipboard || \
           command -v wl-copy &>/dev/null && wl-copy || \
           command -v clip.exe &>/dev/null && clip.exe || \
           echo 'Clipboard copy not supported' >&2 
         }
-      )+abort"
+      )+abort" | awk '{print $1}'
 )
 
 if [ -n "$selected_branch" ]; then
   git switch "$selected_branch" 2>/dev/null || git checkout "$selected_branch"
 fi
+
